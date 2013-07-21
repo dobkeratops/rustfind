d02450bf6d61c7b91b5742327abe10d74b1be9b7
dobkeratops <dobkeratopsgit@gmail.com>
2013-07-21 16:55:12 +0100

    adding pretty-print of rust ast, 'myToStr()' not complete yet but seems to show top level items

--- main.rs
+++ main.rs
@@ -13,7 +13,18 @@ use std::os;
 use std::local_data;
 use extra::json::ToJson;
 
+macro_rules! logi{ 
+	()=>(println(file!()+":"+line!().to_str()+": "));
+	($a:expr)=>(println(file!()+":"+line!().to_str()+": "+$a.to_str()));
+	($a:expr,$b:expr)=>(println(file!()+":"+line!().to_str()+": "+$a.to_str()+$b.to_str()));
+	($a:expr,$b:expr,$c:expr)=>(println(file!()+":"+line!().to_str()+": "+$a.to_str+$b.to_str(),$c.to_str()))
+
+}
+
+
 pub static ctxtkey: local_data::Key<@DocContext> = &local_data::Key;
+
+
 
 struct DocContext {
     crate: @ast::crate,
@@ -21,8 +32,9 @@ struct DocContext {
     sess: driver::session::Session
 }
 
-/// Parses, resolves, and typechecks the given crate
+/// Parses, resolves the given crate
 fn get_ast_and_resolve(cpath: &Path, libs: ~[Path]) -> DocContext {
+
     let parsesess = parse::new_parse_sess(None);
     let sessopts = @driver::session::options {
         binary: @"rustdoc",
@@ -30,7 +42,6 @@ fn get_ast_and_resolve(cpath: &Path, libs: ~[Path]) -> DocContext {
         addl_lib_search_paths: @mut libs,
         .. copy (*rustc::driver::session::basic_options())
     };
-
 
     let diagnostic_handler = syntax::diagnostic::mk_handler(None);
     let span_diagnostic_handler =
@@ -44,18 +55,126 @@ fn get_ast_and_resolve(cpath: &Path, libs: ~[Path]) -> DocContext {
                                                      &driver::driver::file_input(cpath.clone()),
                                                      driver::driver::cu_typeck, None);
                                                      
-    DocContext { crate: crate.unwrap(), tycx: tycx.unwrap(), sess: sess }
+	let c=crate.unwrap();
+	let t=tycx.unwrap();
+    DocContext { crate: c, tycx: t, sess: sess }
+}
+
+trait MyToStr {
+	fn myToStr(&self)->~str;
+}
+impl<A:MyToStr,B:MyToStr> MyToStr for (A,B) {
+	fn myToStr(&self)->~str{ let &(ref a, ref b)=self; ~"("+a.myToStr()+~","+b.myToStr()+~")"}
+}
+impl MyToStr for ast::mutability {
+	fn myToStr(&self)->~str{
+		match(self) {
+			&m_mutbl=>~"m_mutbl",
+//			m_imm=>~"m_imm",
+//			m_const=>~"m_const",
+		}
+	}
+}
+/*
+impl<'self> MyToStr for &ast::mutability<'self> {
+	fn myToStr(&'self self)->~str{
+		match(self) {
+			&m_mutbl=>~"m_mutbl",
+//			m_imm=>~"m_imm",
+//			m_const=>~"m_const",
+		}
+	}
+}
+*/
+
+impl MyToStr for int {
+	fn myToStr(&self)->~str{self.to_str()}
+}
+impl MyToStr for ast::expr {
+	fn myToStr(&self)->~str { 
+		"{"+"id:"+self.id.myToStr()+",node:"+self.node.myToStr()+",span:"+self.span.myToStr()+"}"
+	}
+}
+impl<T:MyToStr> MyToStr for Option<T> {
+	fn myToStr(&self)->~str{
+		match(self) {
+			&Some(ref x)=>~"Some("+x.myToStr()+")",
+			&None=>~"None"
+		}
+	}
+}
+impl MyToStr for syntax::codemap::BytePos {
+	fn myToStr(&self)->~str {
+		let &syntax::codemap::BytePos(x)=self; x.to_str()
+	}
+}
+impl MyToStr for @syntax::codemap::ExpnInfo {
+	fn myToStr(&self)->~str{~"ExpnInfo{TODO}"}
+}
+impl MyToStr for syntax::codemap::ExpnInfo {
+	fn myToStr(&self)->~str{~"ExpnInfo{TODO}"}
+}
+
+fn fMyToStr(x:&syntax::codemap::ExpnInfo)->~str {
+	x.myToStr()
+}
+
+impl MyToStr for @ast::expr {
+	fn myToStr(&self)->~str {
+		~"{"+~"id:"+self.id.to_str()+~",node:"+self.node.myToStr()+~",span"+self.span.myToStr()+~"}"
+	}
+}
+
+impl MyToStr for syntax::codemap::span {
+	fn myToStr(&self)->~str {
+		~"{"+~"lo:"+self.lo.myToStr()+~",hi:"+self.hi.myToStr()+~",expn_info:"+self.expn_info.myToStr()+~"}"
+	}
+}
+
+impl MyToStr for ast::item_ {
+	fn myToStr(&self)->~str{
+		use syntax::ast::*;
+		match (self) {
+			&item_static(ref t,ref m,ref e)=> ~"item_static"+(*m,*e).myToStr()+~")",
+			&item_fn(ref decl, ref purity,ref AbiSet, ref Generics, ref refblk)=>
+				~"item_fn()",
+			&item_mod(ref m) => ~"item_mod()",
+			&item_foreign_mod(ref m) => ~"item_foreign_mod()",
+			&item_ty(ref _ty,ref g) => ~"item_ty()",
+			&item_enum(ref e,ref g) => ~"item_enum()",
+			&item_struct(ref sd,ref g) => ~"item_struct()",
+			&item_trait(ref g, ref trs,ref tms)=>~"item_trait",
+			&item_impl(ref g, ref opt_tr, ref ty, ref ms)=>~"item_impl",
+			&item_mac(ref m)=>~"item_mac()",
+
+		}
+	}
+}
+impl MyToStr for ast::expr_ {
+	fn myToStr(&self)->~str {
+		match(self) {
+			_ =>~"expr_(TODO)" // all the complexity of statements is handled here
+		}
+	}
+}
+
+fn my_visit(c:&ast::crate){
+
+	for c.node.module.items.iter().advance|x|{
+		println(x.node.myToStr());		
+	}
 }
 
 fn main() {
     use extra::getopts::*;
     use std::hashmap::HashMap;
 
-	println("Rustfind:-");
     let args = os::args();
+
     let opts = ~[
         optmulti("L")
     ];
+	
     let matches = getopts(args.tail(), opts).get();
     let libs = opt_strs(&matches, "L").map(|s| Path(*s));
 
@@ -65,6 +184,8 @@ fn main() {
         debug!("%?: %?", k, v);
     }
     local_data::set(ctxtkey, ctxt);
+//	println(ctxt.to_str());
+	my_visit(ctxt.crate);
 /*
     let mut v = @mut RustdocVisitor::new();
     v.visit(ctxt.crate);
--- makefile
+++ makefile
@@ -1,5 +1,7 @@
 test : rustfind
-	./rustfind rustfind.rs
+	@if [ ! $(RUST) ] ; then echo "set RUST to point to root of rust sourcetree" ; fi
+
+	./main rustfind.rs -L $(RUST)/x86_64-unknown-linux-gnu/stage2/lib
 
 rustfind: main.rs rustfind.rs astdump.rs
 	rustc main.rs
