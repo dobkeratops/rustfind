4d6eda5ece5b5535a612d1377aef495fbd8140e0
dobkeratops <dobkeratopsgit@gmail.com>
2013-07-22 00:57:05 +0100

    dump! macro, using stringify and %?, automates what we used to do manually in c

--- main.rs
+++ main.rs
@@ -14,17 +14,102 @@ use std::local_data;
 use extra::json::ToJson;
 
 macro_rules! logi{ 
-	()=>(println(file!()+":"+line!().to_str()+": "));
-	($a:expr)=>(println(file!()+":"+line!().to_str()+": "+$a.to_str()));
-	($a:expr,$b:expr)=>(println(file!()+":"+line!().to_str()+": "+$a.to_str()+$b.to_str()));
-	($a:expr,$b:expr,$c:expr)=>(println(file!()+":"+line!().to_str()+": "+$a.to_str+$b.to_str(),$c.to_str()))
-
+	($($a:expr),*)=>(println(file!()+":"+line!().to_str()+": " $(+$a.to_str())* ))
 }
-macro_rules! dump{ ($a:expr)=>(logi!(fmt!("%?",$a));)}
+//macro_rules! dump{ ($a:expr)=>(logi!(fmt!("%s=%?",stringify!($a),$a).indent(2,160));)}
+fn newline_if_over(a:~str,l:uint)->~str{if a.len()>l {a+~"\n"}else{a}}
+macro_rules! dump{ ($($a:expr),*)=>
+	(	{	let mut txt=~""; 
+			$( txt=txt.append(
+				fmt!("%s=%?",stringify!($a),$a)+~",") 
+			);*; 
+			logi!(txt.indent(2,160)); 
+		}
+	)
+}
 
 pub static ctxtkey: local_data::Key<@DocContext> = &local_data::Key;
 
+/// tags: pretty print,code formatting,brace indentation, json braces, brackets,nesting
+trait Indent {
+	fn indent(&self,tab:int,line:int)->Self;
+}
 
+impl Indent for ~str {
+	fn indent(&self,tabsize:int, linesize:int)->~str {
+		// todo-write as iterators.
+		fn change_indent(c:u8)->int{
+			match c as char {
+				'{'|'('|'['=> 1,
+				'}'|')'|']'=> -1,
+				_ => 0
+			}
+		}			
+		let mut a:~str=~"";
+		let mut i=0;
+		let mut indent=0;
+		let len=self.len();
+		while i<len {
+			// skip leading whitespace. we are on a newline.
+			while i<len && (self[i]==' 'as u8 || self[i]=='\t'as u8) { i+=1;}
+			// measure line size from here
+			let mut dii=0;
+			if change_indent(self[i])<0 { indent-=1;dii=1}/*TODO-more elegant*/
+			let mut cur_linesize=indent*tabsize;
+			let mut ii=i;
+			let mut inner_brace_level=indent;
+			let mut last_open=len;
+			let mut first_base_delim=len;
+			
+			while ii<len && inner_brace_level>=indent {
+				let c=self[ii];
+				if c=='\n'as u8 {break};
+				if cur_linesize >= linesize {break};
+				cur_linesize+=1;
+				let di=change_indent(c);
+				if di>0 && inner_brace_level==indent {last_open=ii;};
+				inner_brace_level+=di;
+				if inner_brace_level==indent {
+					if (di<0 ||c==','as u8||c==';'as u8){last_open=ii;};
+ 
+					if c==','as u8||c==';'as u8 && first_base_delim==len{	
+						//first_base_delim=ii;
+					}
+				}
+				ii+=1
+			}
+			{
+				let mut ii=0;
+				while ii<tabsize*indent { a.push_char(' '); ii+=1 }
+			}
+			indent+=dii;	// the extra one we considered.
+			let init_indent=indent;
+
+			if cur_linesize<linesize 
+			{
+				// copy the line. we dont overstep
+				while i<len && self[i]!='\n'as u8 && indent>=init_indent && i<=first_base_delim && i<=last_open{
+					let c=self[i];
+					indent+=change_indent(c);
+					a.push_char(c as char);
+					i+=1;
+				}
+				if self[i]=='\n'as u8 {ii+=1;}
+			}
+			else {
+				// copy the line until the lines' last opening
+				while i<len && indent>=init_indent && i<=last_open && i<=first_base_delim{
+					let c=self[i];
+					indent+=change_indent(c);
+					a.push_char(c as char);
+					i+=1;
+				}
+			}
+			a.push_char('\n');
+		}
+		a
+	}
+}
 
 struct DocContext {
     crate: @ast::crate,
@@ -32,6 +117,7 @@ struct DocContext {
     sess: driver::session::Session
 }
 
+/// tags: crate,ast,parse resolve
 /// Parses, resolves the given crate
 fn get_ast_and_resolve(cpath: &Path, libs: ~[Path]) -> DocContext {
 
@@ -250,15 +336,16 @@ fn main() {
     let opts = ~[
         optmulti("L")
     ];
-	
+
     let matches = getopts(args.tail(), opts).get();
     let libs = opt_strs(&matches, "L").map(|s| Path(*s));
-
+	dump!(matches,libs,args);
     let ctxt = @get_ast_and_resolve(&Path(matches.free[0]), libs);
     debug!("defmap:");
-//    for ctxt.tycx.def_map.iter().advance |(k, v)| {
+    for ctxt.tycx.def_map.iter().advance |(k, v)| {
+		dump!(k,v);
 //        debug!("%?: %?", k, v);
-//    }
+    }
     local_data::set(ctxtkey, ctxt);
 //	println(ctxt.to_str());
 	//my_visit(ctxt.crate);
--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 test : rustfind
 	@if [ ! $(RUST) ] ; then echo "set RUST to point to root of rust sourcetree" ; fi
 
-	./main rustfind.rs -L $(RUST)/x86_64-unknown-linux-gnu/stage2/lib
+	./main test_input.rs -L $(RUST)/x86_64-unknown-linux-gnu/stage2/lib
 
 rustfind: main.rs rustfind.rs astdump.rs
 	rustc main.rs
