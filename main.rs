a0fde964d733a2bbbff572b7c2642f1575ce53fd
dobkeratops <dobkeratopsgit@gmail.com>
2013-07-22 15:42:45 +0100

    got nodes from source location, but many details remain..

--- main.rs
+++ main.rs
@@ -1,6 +1,5 @@
 extern mod syntax;
 extern mod rustc;
-
 extern mod extra;
 
 use rustc::{front, metadata, driver, middle};
@@ -8,13 +7,20 @@ use rustc::{front, metadata, driver, middle};
 use syntax::parse;
 use syntax::ast;
 use syntax::ast_map;
+use syntax::visit;
+use syntax::visit::*;
+use syntax::visit::{Visitor, fn_kind};
+
+use syntax::abi::AbiSet;
+use syntax::ast;
+use syntax::codemap::span;
 
 use std::os;
 use std::local_data;
 use extra::json::ToJson;
 
 macro_rules! logi{ 
-	($($a:expr),*)=>(println(file!()+":"+line!().to_str()+": " $(+$a.to_str())* ))
+	($($a:expr),*)=>(println((file!()+":"+line!().to_str()+": " $(+$a.to_str())*) .indent(2,160)))
 }
 //macro_rules! dump{ ($a:expr)=>(logi!(fmt!("%s=%?",stringify!($a),$a).indent(2,160));)}
 fn newline_if_over(a:~str,l:uint)->~str{if a.len()>l {a+~"\n"}else{a}}
@@ -23,7 +29,7 @@ macro_rules! dump{ ($($a:expr),*)=>
 			$( txt=txt.append(
 				fmt!("%s=%?",stringify!($a),$a)+~",") 
 			);*; 
-			logi!(txt.indent(2,160)); 
+			logi!(txt); 
 		}
 	)
 }
@@ -327,6 +333,317 @@ fn my_visit(c:&ast::crate){
 	}
 }
 
+#[deriving(Clone)]
+struct MyData {
+	depth:int,
+}
+
+fn makeMyVisitor()->syntax::visit::vt<@MyData>
+{
+	use syntax::visit::*;
+	use syntax::ast::*;
+	use syntax::codemap::*;
+/*	
+	pub fn visit_fn(fk:&fn_kind,fd:&fn_decl,b:&blk,s:span,nid:node_id, evt:(E,vt<E>)) 
+	{
+		dump!(fk,fd,b,s,nid,evt);
+	};
+*/
+ 	fn my_visit_fn<E>(item: &ast::item, fd: &ast::fn_decl, purity: &ast::purity,
+                     abi: &syntax::abi::AbiSet, gen: &ast::Generics, rcx: E) 
+	{
+		dump!(item,fd,purity,abi,gen,rcx);
+	};
+
+	fn my_visit_item(item: @ast::item, (rcx, vt): (@mut MyData, syntax::visit::vt<@mut MyData>)) 
+	{
+		dump!(item);
+		match item.node {
+	/*
+		    ast::item_mod(ref m) => {
+		        for m.items.iter().advance |i| { (vt.visit_item)(*i, (rcx.clone(), vt)); }
+		    },
+		    ast::item_enum(ref ed, ref gen) => visit_enum_def(item, ed, gen, rcx),
+		    ast::item_struct(sd, ref gen) => visit_struct_def(item, sd, gen, rcx),
+		    ast::item_fn(ref fd, ref pur, ref abi, ref gen, _) =>
+		        visit_fn(item, fd, pur, abi, gen, rcx),
+	*/            _ => (),
+		}
+
+	}
+
+	type Vct=(@ MyData, syntax::visit::vt<@ MyData>);
+	let v=@Visitor {
+/*		visit_item:
+		|item: @ast::item, (rcx, vt): Vct| {
+			dump!();
+
+
+		},
+*/
+		visit_fn:
+		|fnk:&fn_kind, fd: &ast::fn_decl, blk:&spanned<blk_>,  sp:syntax::codemap::span,
+                     nid:node_id, (rcx,vt): Vct| 
+		{
+			dump!(fnk,fd,sp,blk,nid);
+		},
+
+
+		..*visit::default_visitor::<@ MyData>()
+	};
+	mk_vt(v)
+}
+
+/*
+pub enum item_ {
+    item_static(Ty, mutability, @expr),
+    item_fn(fn_decl, purity, AbiSet, Generics, blk),
+    item_mod(_mod),
+    item_foreign_mod(foreign_mod),
+    item_ty(Ty, Generics),
+    item_enum(enum_def, Generics),
+    item_struct(@struct_def, Generics),
+    item_trait(Generics, ~[trait_ref], ~[trait_method]),
+    item_impl(Generics,
+              Option<trait_ref>, // (optional) trait this impl implements
+              Ty, // self
+              ~[@method]),
+    // a macro invocation (which includes macro definition)
+    item_mac(mac),
+pub struct struct_def {
+    fields: ~[@struct_field], // fields, not including ctor 
+    ctor_id: Option<node_id>
+pub struct spanned<T> { node: T, span: span }
+pub type struct_field = spanned<struct_field_>;
+pub struct struct_field_ {
+    kind: struct_field_kind,
+    id: node_id,
+    ty: Ty,
+    attrs: ~[attribute],
+}
+
+*/
+/*
+impl MyAstNode {
+	fn span(&self)->syntax::codemap::span {
+		match self {
+			&ViewItem(ref x)=> x.span,
+			&Item(ref x)=> x.span,
+			&Expr(ref x)=> x.span,
+		}
+	}
+}
+
+enum WhatNext {
+	Break,
+	Continue,
+	Recurse
+}
+
+type MyAstFn<A> = @fn(a:A, node:@MyAstNode,depth:int)->(A,WhatNext);
+
+priv fn v_ast_item<A:Clone>(mut acc:A, it:@ast::item,f:MyAstFn<A>,depth:int)->(A,WhatNext){
+	match it.node {
+	ast::item_mod(x)=> v_ast_module(acc.clone(),@x,f,depth+1),
+//	item_struct(x)=>{}
+	_=>(acc,Continue)
+	}
+}
+
+priv fn v_ast_module<A:Clone>(mut acc:A, m:@ast::_mod,f:MyAstFn<A>,depth:int)->(A,WhatNext){
+	for m.view_items.iter().advance |vi|{
+		let bvi=@copy *vi;
+		let (a,c)=f(acc.clone(),@ViewItem(bvi),depth);
+		acc=a;
+	}
+	for m.items.iter().advance |it|{
+		let (a,c)=f(acc.clone(),@Item(*it),depth);
+		acc=a;
+		v_ast_item(acc.clone(),*it,f,depth);
+	}
+	(acc,Continue)
+}
+
+pub fn my_visit_ast<A:Clone>(input:A, c:@ast::crate, f:MyAstFn<A>,depth:int)->(A,WhatNext) {
+	let cm=@copy c.node.module;
+	let (input1,_)=v_ast_module(input, cm,f,depth);
+	(input1,Continue)
+}
+
+fn find_node_at(src_loc:int,c:@ast::crate)->~[@MyAstNode] 
+{
+	// my_visit_ast(~[], c, |...|{
+	//     if span encloses this location
+    //          push this node onto Accumulator
+	//          recurse
+	//     else
+    //          continue
+	//	}
+	~[]
+}
+*/
+
+mod find_ast_node 
+{
+	use syntax::ast::*;
+	use syntax::visit::*;
+	use syntax::codemap::*;
+
+	/// main
+	pub fn find(c:@crate,_location:uint)->~[AstNode] {
+		let mut s= @mut State{
+			result:~[], location:_location, stop:false
+		};
+		let vt=mk_vt(@Visitor{
+			visit_item:f_item,
+			visit_struct_field:f_struct_field,
+			visit_expr:f_expr,
+			visit_pat:f_pat,
+			visit_decl:f_decl,
+			//visit_ty:f_ty,
+
+			.. *default_visitor::<@mut State>()
+			}
+		);
+		visit_crate(c, (s,vt));
+		s.result.clone()
+	}
+
+
+	#[deriving(Clone)]
+	pub enum AstNode
+	{
+		// struct Visitor<E>.visit_mod: @fn(&_mod, span, node_id, (E, vt<E>)),
+		// struct Visitor<E>.visit_view_item: @fn(&view_item, (E, vt<E>)),
+		astnode_view_item(@view_item),
+		// struct Visitor<E>.visit_foreign_item: @fn(@foreign_item, (E, vt<E>)),
+		// struct Visitor<E>.visit_item: @fn(@item, (E, vt<E>)),
+		astnode_item(@item),
+		// struct Visitor<E>.visit_local: @fn(@local, (E, vt<E>)),
+		// struct Visitor<E>.visit_block: @fn(&blk, (E, vt<E>)),
+		// struct Visitor<E>.visit_stmt: @fn(@stmt, (E, vt<E>)),
+		// struct Visitor<E>.visit_arm: @fn(&arm, (E, vt<E>)),
+		// struct Visitor<E>.visit_pat: @fn(@pat, (E, vt<E>)),
+		astnode_pat(@pat),
+		// struct Visitor<E>.visit_decl: @fn(@decl, (E, vt<E>)),
+		astnode_decl(@decl),
+		// struct Visitor<E>.visit_expr: @fn(@expr, (E, vt<E>)),
+		astnode_expr(@expr),
+		// struct Visitor<E>.visit_expr_post: @fn(@expr, (E, vt<E>)),
+		astnode_expr_post(@expr),
+		// struct Visitor<E>.visit_ty: @fn(&Ty, (E, vt<E>)),
+		astnode_ty(@Ty),
+		// struct Visitor<E>.visit_generics: @fn(&Generics, (E, vt<E>)),
+		// struct Visitor<E>.visit_fn: @fn(&fn_kind, &fn_decl, &blk, span, node_id, (E, vt<E>)),
+		// struct Visitor<E>.visit_ty_method: @fn(&ty_method, (E, vt<E>)),
+		astnode_ty_method(@ty_method),
+		// struct Visitor<E>.visit_trait_method: @fn(&trait_method, (E, vt<E>)),
+		astnode_trait_method(@trait_method),
+		// struct Visitor<E>.visit_struct_def: @fn(@struct_def, ident, &Generics, node_id, (E, vt<E>)),
+		astnode_struct_def(@struct_def),
+		// struct Visitor<E>.visit_struct_field: @fn(@struct_field, (E, vt<E>)),
+		astnode_struct_field(@struct_field),
+
+		astnode_empty	
+	}
+
+	struct State {
+		result: ~[AstNode],		// todo - full tree path, all the parent nodes.
+		location: uint,
+		stop: bool
+	}
+	fn span_contains(l:uint,s:span)->bool {
+		let BytePos(lo)=s.lo;
+		let BytePos(hi)=s.hi;
+		l>=lo && l<hi
+	}
+
+// struct Visitor<E>.visit_mod: @fn(&_mod, span, node_id, (E, vt<E>)),
+// struct Visitor<E>.visit_view_item: @fn(&view_item, (E, vt<E>)),
+// struct Visitor<E>.visit_foreign_item: @fn(@foreign_item, (E, vt<E>)),
+// struct Visitor<E>.visit_item: @fn(@item, (E, vt<E>)),
+	fn f_item(a:@item, (s,v):(@mut State, vt<@mut State>)) {
+		if span_contains(s.location, a.span) {
+			s.result.push(astnode_item(a));
+			let found=a.span;
+			dump!(found)
+		}
+		visit_item(a,(s,v))
+	}
+// struct Visitor<E>.visit_local: @fn(@local, (E, vt<E>)),
+// struct Visitor<E>.visit_block: @fn(&blk, (E, vt<E>)),
+// struct Visitor<E>.visit_stmt: @fn(@stmt, (E, vt<E>)),
+// struct Visitor<E>.visit_arm: @fn(&arm, (E, vt<E>)),
+// struct Visitor<E>.visit_pat: @fn(@pat, (E, vt<E>)),
+	fn f_pat(a:@pat, (s,v):(@mut State, vt<@mut State>)) {
+		if span_contains(s.location, a.span) {
+			s.result.push(astnode_pat(a));
+			let found=a.span;
+			dump!(found)
+		}
+		visit_pat(a,(s,v))
+	}
+
+
+// struct Visitor<E>.visit_decl: @fn(@decl, (E, vt<E>)),
+	fn f_decl(a:@decl, (s,v):(@mut State, vt<@mut State>)) {
+		if span_contains(s.location, a.span) {
+			s.result.push(astnode_decl(a));
+			let found=a.span;
+			dump!(found)
+		}
+		visit_decl(a,(s,v))
+	}
+
+
+// struct Visitor<E>.visit_expr: @fn(@expr, (E, vt<E>)),
+	fn f_expr(a:@expr, (s,v):(@mut State, vt<@mut State>)) {
+		if span_contains(s.location, a.span) {
+			s.result.push(astnode_expr(a));
+			let found=a.span;
+			dump!(found)
+		}
+		visit_expr(a,(s,v))
+	}
+
+// struct Visitor<E>.visit_expr_post: @fn(@expr, (E, vt<E>)),
+	fn f_expr_post(a:@expr, (s,v):(@mut State, vt<@mut State>)) {
+		if span_contains(s.location, a.span) {
+			s.result.push(astnode_expr_post(a));
+			let found=a.span;
+			dump!(found)
+		}
+		visit_expr(a,(s,v))
+	}
+
+// struct Visitor<E>.visit_ty: @fn(&Ty, (E, vt<E>)),
+	fn f_ty(a:@Ty, (s,v):(@mut State, vt<@mut State>)) {
+		if span_contains(s.location, a.span) {
+			s.result.push(astnode_ty(a));
+			let found=a.span;
+			dump!(found)
+		}
+		visit_ty(a,(s,v))
+	}
+
+// struct Visitor<E>.visit_generics: @fn(&Generics, (E, vt<E>)),
+// struct Visitor<E>.visit_fn: @fn(&fn_kind, &fn_decl, &blk, span, node_id, (E, vt<E>)),
+
+// struct Visitor<E>.visit_ty_method: @fn(&ty_method, (E, vt<E>)),
+// struct Visitor<E>.visit_trait_method: @fn(&trait_method, (E, vt<E>)),
+// struct Visitor<E>.visit_struct_def: @fn(@struct_def, ident, &Generics, node_id, (E, vt<E>)),
+// struct Visitor<E>.visit_struct_field: @fn(@struct_field, (E, vt<E>)),
+	fn f_struct_field(a:@struct_field, (s,v):(@mut State, vt<@mut State>)) {
+		if span_contains(s.location, a.span) {
+			s.result.push(astnode_struct_field(a));
+			let found=a.span;
+			dump!(found)
+		}
+		visit_struct_field(a,(s,v))
+	}
+
+}
+
 fn main() {
     use extra::getopts::*;
     use std::hashmap::HashMap;
@@ -336,26 +653,14 @@ fn main() {
     let opts = ~[
         optmulti("L")
     ];
-
     let matches = getopts(args.tail(), opts).get();
     let libs = opt_strs(&matches, "L").map(|s| Path(*s));
-	dump!(matches,libs,args);
+	dump!(args,matches);
+	dump!(libs);
     let ctxt = @get_ast_and_resolve(&Path(matches.free[0]), libs);
-    debug!("defmap:");
-    for ctxt.tycx.def_map.iter().advance |(k, v)| {
-		dump!(k,v);
-//        debug!("%?: %?", k, v);
-    }
-    local_data::set(ctxtkey, ctxt);
-//	println(ctxt.to_str());
-	//my_visit(ctxt.crate);
-//	println(ctxt.crate.myToStr());
-	dump!(ctxt.crate);
-/*
-    let mut v = @mut RustdocVisitor::new();
-    v.visit(ctxt.crate);
 
-    let mut crate = v.clean();
-    println(crate.to_json().to_str());
-*/
+    local_data::set(ctxtkey, ctxt);
+	dump!(find_ast_node::find(ctxt.crate,25))
+	dump!(find_ast_node::find(ctxt.crate,97))
+
 }
--- makefile
+++ makefile
@@ -1,7 +1,8 @@
 test : rustfind
 	@if [ ! $(RUST) ] ; then echo "set RUST to point to root of rust sourcetree" ; fi
+	echo $(RUST)
 
-	./main test_input.rs -L $(RUST)/x86_64-unknown-linux-gnu/stage2/lib
+	./main test_input.rs -L ~/gplsrc/rust/x86_64-unknown-linux-gnu/stage2/lib
 
 rustfind: main.rs rustfind.rs astdump.rs
 	rustc main.rs
